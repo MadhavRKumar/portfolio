62e055e7de15722ff21e588b8f3b321db73be95b
MadhavRKumar <madhavrkumar@gmail.com>
2020-04-26 04:45:33 -0400

    Add art, display all art in folder

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,15 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const Art = ({data}) => (
   <>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Art" />
+	{data.allFile.edges.map(img => {
+		return <Img resolutions={img.node.childImageSharp.resolutions} />})}
   </>
 )
 
-export default SecondPage
+export default Art
+
+export const query = graphql`
+	query ImagesQuery {
+		allFile(filter: {sourceInstanceName: {eq: "art"}}) {
+			edges {
+				node {
+					childImageSharp{
+						resolutions(width:125, height:125) {
+						...GatsbyImageSharpResolutions
+						}
+					}
+				}
+			}
+		}
+	}`
