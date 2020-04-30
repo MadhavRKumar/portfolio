2bda2a06b62eedcbf85079cc0c051a6bb1a5e250
MadhavRKumar <madhavrkumar@gmail.com>
2020-04-30 18:56:38 -0400

    Place art in correct foler

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `art`,
+        path: `${__dirname}/src/art/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
