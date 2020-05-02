ed0f5f4546cf985a90fa2eb95b85fc8a0dfea2dc
MadhavRKumar <madhavrkumar@gmail.com>
2020-05-02 01:23:43 -0400

    Add padding and labels to art

--- src/pages/art.js
+++ src/pages/art.js
@@ -4,22 +4,33 @@ import Img from "gatsby-image"
 import SEO from "../components/seo"
 
 const Art = ({data}) => (
-  <>
-    <SEO title="Art" />
+	<> 
+	<SEO title="Art" />
+	<div className="flex-wrapper">
 	{data.allFile.edges.map(img => {
-		return <Img resolutions={img.node.childImageSharp.resolutions} />})}
-  </>
+		return <LabeledImage image={img} />})}
+	</div>
+	</>
 )
 
 export default Art
+
+const LabeledImage = ({image}) => ( 
+	<div className="art-image"> 
+	<p className="art-label">{image.node.name}</p>
+	<Img resolutions={image.node.childImageSharp.resolutions} />
+	</div>
+) 
+
 
 export const query = graphql`
 	query ImagesQuery {
 		allFile(filter: {sourceInstanceName: {eq: "art"}}) {
 			edges {
 				node {
+					name
 					childImageSharp{
-						resolutions(width:125, height:125) {
+						resolutions(width:375, height:375) {
 						...GatsbyImageSharpResolutions
 						}
 					}
