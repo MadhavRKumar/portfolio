ea8622c1aac141921b66c39b53dff886a16e8260
MadhavRKumar <madhavrkumar@gmail.com>
2020-05-19 04:15:02 -0400

    Fix build issue
    Deleted any empty files and bumped up the version of gatsby

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
 		<Layout title="Contact">
 		<Form onChange={setText}/>	
 		<P5Wrapper sketch={sketch} text={text}/>
-		</Layout>) : (<div></div>);
+		</Layout>) : null;
 }
 
 function sketch (p) {
