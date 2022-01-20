74c8a1afe4b6fb9ea144fc84a971de4696277d37
MadhavRKumar <madhavrkumar@gmail.com>
2022-01-19 23:36:02 -0500

    Style homepage; add react-pixi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import { Link } from 'react-router-dom';
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  return ( 
-	  <nav>
-	  	<header className=""> 
-			<h1>Madhav Kumar</h1> 
-	  	</header>
-	  	<Link to="/about">About</Link>
-	  	<Link to="/projects">Projects</Link>
-	  </nav>
-  );
+	return (
+		<>
+			<nav className="nav">
+				<header className="nav__title">
+					<h1>madhav kumar</h1>
+				</header>
+				<ul className="nav__list">
+					<li className="nav__list-item">
+						<Link className="nav__list-item__link" to="/about">
+							/about/
+						</Link>
+					</li>
+					<li className="nav__list-item">
+						<Link className="nav__list-item__link" to="/projects">
+							/projects/
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</>
+	);
 }
 
 export default App;
